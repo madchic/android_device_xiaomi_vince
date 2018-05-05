dbf80815910dcff022f355708a59151724d978d7
Reiner Zimmermann <reiner.zimmermann@hensoldt.net>
2018-05-05 14:03:15 +0200

    RZ settings

--- full_vince.mk
+++ full_vince.mk
@@ -14,6 +14,16 @@
 # limitations under the License.
 #
 
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/rz-edv.de/security/releasekey
+PRODUCT_PROPERTY_OVERRIDES := \
+        net.dns1=85.214.20.141 \
+        net.dns2=213.73.91.35
+
+GAPPS_VARIANT := pico
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+GAPPS_FORCE_MATCHING_DPI := true
+GAPPS_EXCLUDED_PACKAGES := CalSync
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -28,3 +38,31 @@ PRODUCT_NAME := full_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_PACKAGES += Snap
+PRODUCT_PACKAGES += K9Mail
+PRODUCT_PACKAGES += BarcodeScanner
+PRODUCT_PACKAGES += F-Droid
+PRODUCT_PACKAGES += FDroidPE
+PRODUCT_PACKAGES += Amaze
+PRODUCT_PACKAGES += DAVdroid
+PRODUCT_PACKAGES += OpenTasks
+PRODUCT_PACKAGES += KeePassDroid
+PRODUCT_PACKAGES += Conversations
+PRODUCT_PACKAGES += SatStat
+PRODUCT_PACKAGES += Nextcloud
+PRODUCT_PACKAGES += Wi-FiPrivacyPolice
+PRODUCT_PACKAGES += Eleven
+PRODUCT_PACKAGES += YahooWeatherProvider
+PRODUCT_PACKAGES += UnifiedNlp
+PRODUCT_PACKAGES += LocalGsmNlpBackend
+PRODUCT_PACKAGES += LocalWifiNlpBackend
+PRODUCT_PACKAGES += ULogger
+PRODUCT_PACKAGES += OsmAnd
+PRODUCT_PACKAGES += MaterialPlayer
+PRODUCT_PACKAGES += SWEBrowser
+PRODUCT_PACKAGES += HumbleBundle
+PRODUCT_PACKAGES += OpenCamera
+
+#OpenGapps
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
