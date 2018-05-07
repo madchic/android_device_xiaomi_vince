8f351165cd5beeeaabc430ca4012ce275e0a7dfe
madchic <madchic@users.noreply.github.com>
2018-05-07 22:33:20 +0200

    Update full_vince.mk
    exclude AndroidPlatformServices

--- full_vince.mk
+++ full_vince.mk
@@ -23,6 +23,7 @@ GAPPS_VARIANT := pico
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_MATCHING_DPI := true
 GAPPS_EXCLUDED_PACKAGES := CalSync
+GAPPS_EXCLUDED_PACKAGES += AndroidPlatformServices
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
